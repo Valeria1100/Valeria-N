c1b71e48f91c454ab002387ec83fe035ae599cf8
Valeria1100 <valeria.norena1107@hotmail.com>
2025-02-20 21:50:03 -0500

    Add files via upload

---
+++ Tareagrupal.py
@@ -0,0 +1,16 @@
+#triple de un número
+print("Digite un número")
+num = int(input())
+print("el resultado es ", num*3)
+#cuadruple de un número
+print("Digite un número")
+num = int(input())
+print("el resultado es ", num*4)
+#quintuple de un número
+print("Digite un número")
+num = int(input())
+print("el resultado es ", num*5)
+#sextuple de un número
+print("Digite un número")
+num = int(input())
+print("el resultado es ", num*6)
