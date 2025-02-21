9303d135a718de0953190be8caf71e98008f5c06
Valeria1100 <valeria.norena1107@hotmail.com>
2025-02-20 22:04:49 -0500

    Update Tareagrupal.py

--- Tareagrupal.py
+++ Tareagrupal.py
@@ -1,16 +1,8 @@
-#triple de un número
+#EJEMPLOS
 print("Digite un número")
 num = int(input())
 print("el resultado es ", num*3)
-#cuadruple de un número
-print("Digite un número")
-num = int(input())
 print("el resultado es ", num*4)
-#quintuple de un número
-print("Digite un número")
-num = int(input())
 print("el resultado es ", num*5)
-#sextuple de un número
-print("Digite un número")
-num = int(input())
 print("el resultado es ", num*6)
+print("el resultado es ", num*7)
